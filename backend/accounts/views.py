a15584a73b9b746c8ba0c772bc0f5c2f461d4751
rythm-sachdeva <rythmrko123@gmail.com>
2024-12-28 18:36:58 +0530

    Configured Custom User Authentication and login Using DRF

---
+++ backend/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer,LoginSerialiser
+from rest_framework import status
+from .tokenAuthentication import JWTAuthentication
+
+# Create your views here.
+class RegisterView(APIView):
+    
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+
+    def post(self,request):
+        serializers = LoginSerialiser(data=request.data)
+        if serializers.is_valid():
+            token = JWTAuthentication.generate_token(serializers.validated_data)
+            return Response({'message':'Login Successfull','token':token,'user':serializers.data},status=status.HTTP_201_CREATED)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    
